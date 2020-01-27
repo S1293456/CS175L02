92ea7d82f10ca56e0ed826a794ffc0ae31150a4c
s1293456 <s1293456@cssemaclin19.monmouth.edu>
2020-01-27 14:53:17 -0500

    new commit

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		//This is my first java program
-		System.out.println("Hello World");
+		System.out.println("Hello");
+		System.out.println("");
+		System.out.println("World");
 	}
 
 }
